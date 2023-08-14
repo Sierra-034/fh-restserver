657f1f606c118370180d66c0e651bd9e5e59bcdc
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2023-08-14 17:03:56 -0600

    107: Peticiones HTTP - GET - PUT - POST - DELETE

--- models/server.js
+++ models/server.js
@@ -15,7 +15,33 @@ class Server {
 
     routes() {
         this.app.get('/api', (req, res) => {
-            res.send('Hello World');
+            res.json({
+                msg: 'get API',
+            });
+        });
+
+        this.app.put('/api', (req, res) => {
+            res.json({
+                msg: 'put API',
+            });
+        });
+
+        this.app.post('/api', (req, res) => {
+            res.json({
+                msg: 'post API',
+            });
+        });
+
+        this.app.delete('/api', (req, res) => {
+            res.json({
+                msg: 'delete API',
+            });
+        });
+
+        this.app.patch('/api', (req, res) => {
+            res.json({
+                msg: 'patch API',
+            });
         });
     }
 
